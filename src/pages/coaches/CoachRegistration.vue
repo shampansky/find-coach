4ab4fee4a5cd33fae5264969468fe6b6435d0362
Evgeniy Shadrin <e.shadrin@aristos.pw>
2021-07-18 11:04:59 +0300

    add registration form

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,3 +1,18 @@
 <template>
-  register
+  <section>
+    <base-card>
+      <h2>Register as a coach now</h2>
+      <coaches-form></coaches-form>
+    </base-card>
+  </section>
 </template>
+
+<script>
+import CoachesForm from '../../components/coaches/CoachesForm.vue';
+
+export default {
+  components: {
+    CoachesForm,
+  }
+}
+</script>
