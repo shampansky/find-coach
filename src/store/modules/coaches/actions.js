ce40eaee7885a9e99c993f20761270d59ca5898e
Evgeniy Shadrin <e.shadrin@aristos.pw>
2021-08-17 07:49:00 +0300

    fetching coaches data

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -10,7 +10,7 @@ export default {
       areas: data.areas,
     };
 
-    const response = await fetch(`https://vue-demo-729d8-default-rtdb.europe-west1.firebasedatabase.app/${userId}.json`, {
+    const response = await fetch(`https://vue-demo-729d8-default-rtdb.europe-west1.firebasedatabase.app/coaches/${userId}.json`, {
       method: 'PUT',
       body: JSON.stringify(coachData)
     });
@@ -25,5 +25,31 @@ export default {
       ...coachData,
       id: userId,
     });
+  },
+  async loadCoaches(context) {
+    const response = await fetch(
+      `https://vue-demo-729d8-default-rtdb.europe-west1.firebasedatabase.app/coaches.json`
+    );
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      // error
+    }
+
+    const coaches = [];
+
+    for (const key in responseData) {
+      const coach = {
+        id: key,
+        firstName: responseData[key].firstName,
+        lastName: responseData[key].lastName,
+        description: responseData[key].description,
+        hourlyRate: responseData[key].hourlyRate,
+        areas: responseData[key].areas,
+      };
+      coaches.push(coach);
+    }
+
+    context.commit('setCoaches', coaches);
   }
 };
