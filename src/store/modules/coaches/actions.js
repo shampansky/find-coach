9d3f2a785cbc3287edaa02bd631c78b16abfa83c
Evgeniy Shadrin <e.shadrin@aristos.pw>
2021-08-17 07:13:51 +0300

    store data in firebase

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,7 +1,8 @@
 export default {
-  registerCoach(context,data) {
+  async registerCoach(context,data) {
+    const userId = context.rootGetters.userId;
+
     const coachData = {
-      id: context.rootGetters.userId,
       firstName: data.first,
       lastName: data.last,
       description: data.desc,
@@ -9,6 +10,20 @@ export default {
       areas: data.areas,
     };
 
-    context.commit('registerCoach', coachData);
+    const response = await fetch(`https://vue-demo-729d8-default-rtdb.europe-west1.firebasedatabase.app/${userId}.json`, {
+      method: 'PUT',
+      body: JSON.stringify(coachData)
+    });
+
+    // const responseData = await response.json();
+
+    if (!response.ok) {
+      // error
+    }
+
+    context.commit('registerCoach', {
+      ...coachData,
+      id: userId,
+    });
   }
 };
