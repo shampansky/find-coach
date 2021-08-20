95c23c84d4fdcabd337932c49c3879a01de565ad
Evgeniy Shadrin <e.shadrin@aristos.pw>
2021-08-20 07:47:18 +0300

    get coach requests from firebase

--- src/pages/requests/RequestsReceived.vue
+++ src/pages/requests/RequestsReceived.vue
@@ -1,10 +1,14 @@
 <template>
+<base-dialog :show="!!error" title="An error occurred!" @close="handleError">
+    <p>{{ error }}</p>
+  </base-dialog>
   <section>
     <base-card>
       <header>
         <h2>Requests Recieved</h2>
       </header>
-      <ul v-if="hasRequests">
+      <base-spinner v-if="isLoading"></base-spinner>
+      <ul v-else-if="hasRequests && !isLoading">
         <request-item v-for="req in receivedRequests" :key="req.id" :email="req.userEmail" :message="req.message"></request-item>
       </ul>
       <h3 v-else>You haven't received any requests yet!</h3>
@@ -18,6 +22,12 @@ export default {
   components: {
     RequestItem
   },
+  data() {
+    return {
+      isLoading: false,
+      error: null
+    }
+  },
   computed: {
     receivedRequests() {
       return this.$store.getters['requests/requests'];
@@ -25,6 +35,24 @@ export default {
     hasRequests() {
       return this.$store.getters['requests/hasRequests'];
     }
+  },
+  created() {
+    this.loadRequests();
+  },
+  methods: {
+    async loadRequests() {
+      this.isLoading = true;
+      try {
+        await this.$store.dispatch('requests/fetchRequests');
+      } catch (error) {
+        this.error = error.message || 'Something failed!'; 
+      }
+      
+      this.isLoading = false;
+    },
+    handleError() {
+      this.error = null;
+    }, 
   }
 }
 </script>
